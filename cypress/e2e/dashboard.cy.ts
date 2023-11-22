bc37552ed8f934e50a5d80c596ef5aa00fb274ca
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-11-22 15:37:57 -0600

    fix: update cypress test

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -1,7 +1,7 @@
 describe('Dashboard', () => {
   it('send to config page on first time', () => {
     cy.visit('/');
-    cy.findByText('Configuration settings').should('exist');
+    cy.findByText('Settings').should('exist');
     cy.findByLabelText(/root path/i).type('./repos');
     cy.findByRole('button', { name: /save/i }).click();
     cy.findByRole('link', { name: /dashboard/i }).click();
