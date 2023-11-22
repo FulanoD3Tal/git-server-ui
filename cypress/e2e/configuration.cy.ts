0346f48f90ab0029ae4cb3432ccd8c172640c4d9
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-11-22 15:34:08 -0600

    fix: update cypress test to match new heading

--- cypress/e2e/configuration.cy.ts
+++ cypress/e2e/configuration.cy.ts
@@ -1,7 +1,7 @@
 describe('Configuration flow', () => {
   it('should get and save configuration', () => {
     cy.visit('/config');
-    cy.findByText('Configuration settings').should('exist');
+    cy.findByText('Settings').should('exist');
     cy.findByLabelText(/default branch/i).should('have.value', 'main');
     cy.findByLabelText(/root path/i).type('./');
     cy.findByRole('button', { name: /save/i }).click();
