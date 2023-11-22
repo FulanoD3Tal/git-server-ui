dffa9f1305361ec8ade71a435ba4fd28ce79fc51
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-11-22 15:31:13 -0600

    feat: add redirect to config when first time

--- src/components/organisms/config-form/config-form.tsx
+++ src/components/organisms/config-form/config-form.tsx
@@ -1,6 +1,8 @@
 'use client';
+import { BaseButton } from '@/components/atoms/base-btn';
 import { ConfigField } from '@/components/molecules/config-field/config-field';
 import { useConfigForm } from '@/config/infrastructure/hooks/use-config-form';
+import { AnimatePresence, motion } from 'framer-motion';
 import { FC } from 'react';
 
 type ConfigFormProps = {
@@ -8,13 +10,19 @@ type ConfigFormProps = {
   onSave: (config: PartialConfig) => void;
   /** previous data to should when load */
   previousData?: PartialConfig;
+  /**Loading state */
+  loading?: boolean;
 };
 
 /**
  * Configuration form to save a json file
  * @author [Ing. Roberto Alonso De la Garza Mendoza](https://github.com/FulanoD3Tal)
  */
-export const ConfigForm: FC<ConfigFormProps> = ({ onSave, previousData }) => {
+export const ConfigForm: FC<ConfigFormProps> = ({
+  onSave,
+  previousData,
+  loading = false,
+}) => {
   const {
     form: {
       handleSubmit,
@@ -30,34 +38,37 @@ export const ConfigForm: FC<ConfigFormProps> = ({ onSave, previousData }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-2'>
-      <ConfigField
-        label='Root path'
-        textBoxProps={{
-          ...register('rootPath'),
-          label: 'use a relative path',
-          'aria-label': 'Root path',
-        }}
-        error={Boolean(errors.rootPath)}
-        hitText={errors.rootPath?.message}
-      />
-      <ConfigField
-        label='Default branch'
-        textBoxProps={{
-          ...register('defaultBranch'),
-          label: 'branch to use when create new repos',
-          'aria-label': 'Default branch',
-        }}
-        error={Boolean(errors.defaultBranch)}
-        hitText={errors.defaultBranch?.message}
-      />
-      {/* TODO: move it to the atoms folders */}
-      <button
-        className='bg-black py-1 px-3 rounded-md text-white font-bold'
-        type='submit'
+    <AnimatePresence>
+      <motion.form
+        onSubmit={handleSubmit(onSubmit)}
+        className='flex flex-col gap-2'
       >
-        Save
-      </button>
-    </form>
+        <ConfigField
+          label='Root path'
+          textBoxProps={{
+            ...register('rootPath'),
+            label: 'use a relative path',
+            'aria-label': 'Root path',
+            disabled: loading,
+          }}
+          error={Boolean(errors.rootPath)}
+          hitText={errors.rootPath?.message}
+        />
+        <ConfigField
+          label='Default branch'
+          textBoxProps={{
+            ...register('defaultBranch'),
+            label: 'branch to use when create new repos',
+            'aria-label': 'Default branch',
+            disabled: loading,
+          }}
+          error={Boolean(errors.defaultBranch)}
+          hitText={errors.defaultBranch?.message}
+        />
+        <BaseButton disabled={loading} type='submit' className='mt-5'>
+          {loading ? 'Saving...' : 'Save'}
+        </BaseButton>
+      </motion.form>
+    </AnimatePresence>
   );
 };
--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -2,11 +2,15 @@
 import { useTheme } from '@/theme/infrastructure/hooks/use-theme';
 import React from 'react';
 import { ThemeIconButton } from '../molecules/theme-icon-btn/theme-icon-btn';
+import Link from 'next/link';
 
 export const Header = () => {
   const { theme, toggleSwitch } = useTheme();
   return (
-    <header className='p-1 flex justify-end max-w-7xl mx-auto'>
+    <header className='p-1 flex justify-end items-center gap-4 max-w-7xl mx-auto'>
+      <Link className='hover:underline dark:text-white' href={'/config'}>
+        Config
+      </Link>
       <ThemeIconButton theme={theme} onSwitch={toggleSwitch} />
     </header>
   );
