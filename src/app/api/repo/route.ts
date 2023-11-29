87c7c7977f1705384eb225589a2e6d4907bd59aa
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-11-29 07:44:57 -0600

    fix: add query params to api route repos

--- src/app/api/repo/route.ts
+++ src/app/api/repo/route.ts
@@ -5,7 +5,7 @@ import { ZodError } from 'zod';
 
 export async function GET() {
   try {
-    const repos = await repositoryController.getRepos();
+    const repos = await repositoryController.getRepos({ query: '' });
     return NextResponse.json(repos);
   } catch (error) {
     return NextResponse.json(
@@ -19,7 +19,7 @@ export async function POST(req: NextRequest) {
   const json: NewRepository = await req.json();
 
   try {
-    const { rootPath } =await configurationController.getConfig();
+    const { rootPath } = await configurationController.getConfig();
     const newRepo = { ...json, path: `${rootPath}/${json.name}` };
     const created = await repositoryController.createRepo(newRepo);
     return NextResponse.json(created);
