a83baa1c0aacb1b111c481371ad60f85791148ad
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-12-04 08:53:42 -0600

    feate: get time from last commit fixes #72

--- src/dashboard/infrastructure/repository-git-npm.ts
+++ src/dashboard/infrastructure/repository-git-npm.ts
@@ -3,6 +3,16 @@ import { IRepository } from '../domain/repository-interface';
 import fs from 'fs';
 
 export class RepositorySimpleGit implements IRepository {
+  async getLastUpdated(gitPath: string): Promise<string> {
+    const git = simpleGit({ baseDir: gitPath });
+    let lastUpdated = '';
+    try {
+      lastUpdated = await git.show(['--format="%aI"', '-s']);
+    } catch (error) {
+      lastUpdated = 'No commit yet';
+    }
+    return lastUpdated;
+  }
   async createRepo(
     newRepo: Repository,
     defaultBranch: string
